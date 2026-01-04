3ea713b5571024497e53dc2c6753586c6a23306b
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2026-01-04 12:51:41 +0000

    Initial Commit

---
+++ demorailtest/demorailtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  demorailtestApp.swift
+//  demorailtest
+//
+//  Created by James on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct demorailtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
